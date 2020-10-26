9d6854ab86e065509c167b158c28d545b302c2c8
Patricio Diaz <padiazg@gmail.com>
2020-10-26 20:05:21 -0300

    Updated entrypoint.sh to handle optional labels value, also updated readme.md
    Signed-off-by: Patricio Diaz <padiazg@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,26 @@ echo "Requesting registration URL at '${registration_url}'"
 payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${registration_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 
-./config.sh \
-    --name $(hostname) \
-    --token ${RUNNER_TOKEN} \
-    --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
-    --work ${RUNNER_WORKDIR} \
-    --unattended \
-    --replace \
-    --labels ${RUNNER_LABELS}
+
+if [ -z "$RUNNER_LABELS" ]
+then 
+    ./config.sh \
+        --name $(hostname) \
+        --token ${RUNNER_TOKEN} \
+        --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
+        --work ${RUNNER_WORKDIR} \
+        --unattended \
+        --replace
+else 
+    ./config.sh \
+        --name $(hostname) \
+        --token ${RUNNER_TOKEN} \
+        --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
+        --work ${RUNNER_WORKDIR} \
+        --unattended \
+        --replace \
+        --labels ${RUNNER_LABELS}
+fi
 
 remove() {
     ./config.sh remove --unattended --token "${RUNNER_TOKEN}"
--- readme.md
+++ readme.md
@@ -15,6 +15,19 @@ $ docker run -it --rm \
 padiazg/github-actions-runner:v2.273.5-docker
 ```
 
+You can set labels for the runner as stated in the [docs](https://docs.github.com/es/free-pro-team@latest/actions/hosting-your-own-runners/using-labels-with-self-hosted-runners) passing the `RUNNER_LABELS` variable the the container
+
+```bash
+$ docker run -it --rm \
+-e GITHUB_OWNER=... \
+-e GITHUB_REPOSITORY=... \
+-e GITHUB_PAT=... \
+-e RUNNER_LABELS=prod,linux,x64 \
+-v /var/run/docker.sock:/var/run/docker.sock \
+--name github-actions-runner \
+padiazg/github-actions-runner:v2.273.5-docker
+```
+
 
 References
 https://sanderknape.com/2020/03/self-hosted-github-actions-runner-kubernetes/
